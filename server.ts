1ef61f18c6cc2d918b3abc9c23642b06c602c4ea
Chris McHardy <chris.mchardy@bbd.co.za>
2025-01-20 14:43:57 +0200

    Trial commit

--- server.ts
+++ server.ts
@@ -7,6 +7,7 @@ app.get('/hello', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
 
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
